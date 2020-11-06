8078f7b83987c96e40066bbe44bbb8a64e3a6f1d
Raphoudre <raphael.ivol@hotmail.fr>
2020-11-06 09:12:26 +0100

    ex 3 partie 5

---
+++ script.js
@@ -0,0 +1,19 @@
+function pink(){
+    document.getElementById("div1").className= "pink"
+}
+
+function blue(){
+    document.getElementById("div1").className= "blue"
+}
+
+function purple(){
+    document.getElementById("div1").className= "purple"
+}
+
+function bold(){
+    document.getElementById("div1").className= "bold"
+}
+
+function italic(){
+    document.getElementById("div1").className= "italic"
+}
